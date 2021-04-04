0d2e838ae199666d221ac9cf837e01e53e0a1409
PauloMarcusso <phmarcusso1@gmail.com>
2021-04-03 21:34:48 -0300

    Configurando o Refresh Token Grant Type no Authorization Server

--- src/main/java/com/mandaeauth/AuthorizationServerConfig.java
+++ src/main/java/com/mandaeauth/AuthorizationServerConfig.java
@@ -3,6 +3,7 @@ package com.mandaeauth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -20,22 +21,27 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients
                 .inMemory()
                 .withClient("mandae-web")
                 .secret(passwordEncoder.encode("123"))
-                .authorizedGrantTypes("password")
+                .authorizedGrantTypes("password", "refresh_token")
                 .scopes("write", "read")
                 .accessTokenValiditySeconds(60 * 60 * 6)// 6 horas (padrão é 12 horas);
-        .and()
+                .and()
                 .withClient("checktoken")
                 .secret(passwordEncoder.encode("check123"));
     }
 
     //Para o PasswordFlow, precisamos desse método
     @Override public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManager);
+        endpoints
+                .authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService);
     }
 
     @Override public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
